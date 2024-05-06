72fafdce1a29ddcba01751df46e4ce78fdb43d54
supriyaponugupati-sherwin <supriya.ponugupati@sherwin.com>
2024-05-06 20:05:03 +0530

    Added 2nd file

--- src/test/java/Utils/Constants.java
+++ src/test/java/Utils/Constants.java
@@ -7,8 +7,12 @@ public class Constants {
    // public static final String pdfFileURL = "C:/Users/suponugupati/Documents/State of CA/POC/PDF Files/Emergency Response Referral Document.pdf";
     public static final String SOC_369_Agency_Relative_Guardianship_Disclosure = "SOC_369_Agency_Relative_Guardianship_Disclosure";
     public static final String SOC_369_Agency_Relative_Guardianship_Disclosure_Path = "C:/Users/suponugupati/Documents/State of CA/POC/PDF Files/SOC 369 - Agency-Relative Guardianship Disclosure.pdf";
-    public static String pdfFileURL ="";
-    public static int pageCount = Integer.parseInt("");
+    public static String pdfFileURL = "C:/path/to/your/pdf/file.pdf";
+    public static int pageCount = 0;
+    public static String headerValue = "Header Value";
 
-
+    public static final String STRTP_Admission_Agreement_By_Agency = "STRTP Admission Agreement By Agency (SOC 154C)";
+  public static final String STRTP_Admission_Agreement_By_Agency_Path = "C:/Users/suponugupati/Documents/State of CA/POC/PDF Files/STRTP Admission Agreement By Agency (SOC 154C).pdf";
+ public static final String SOC_369_Agency_Relative_Guardianship_Disclosure_Header = "AGENCY-RELATIVE GUARDIANSHIP";
+    public static final String STRTP_Admission_Agreement_By_Agency_Header ="AGENCY - SHORT-TERM RESIDENTIAL THERAPEUTIC PROGRAM";
 }
