aac219a3d22e13cde9bd0f3a0efdba78a8e888db
supriyaponugupati-sherwin <supriya.ponugupati@sherwin.com>
2024-05-09 15:49:28 +0530

    Added all 4 files

--- src/test/java/Utils/Constants.java
+++ src/test/java/Utils/Constants.java
@@ -15,4 +15,12 @@ public class Constants {
   public static final String STRTP_Admission_Agreement_By_Agency_Path = "C:/Users/suponugupati/Documents/State of CA/POC/PDF Files/STRTP Admission Agreement By Agency (SOC 154C).pdf";
  public static final String SOC_369_Agency_Relative_Guardianship_Disclosure_Header = "AGENCY-RELATIVE GUARDIANSHIP";
     public static final String STRTP_Admission_Agreement_By_Agency_Header ="AGENCY - SHORT-TERM RESIDENTIAL THERAPEUTIC PROGRAM";
+
+    public static final String FP_Agreement_Child_by_Agency_SOC_156_2 = "FP Agreement Child by Agency (SOC 156)-2";
+    public static final String FP_Agreement_Child_by_Agency_SOC_156_2_Path = "C:/Users/suponugupati/Documents/State of CA/POC/PDF Files/FP Agreement Child by Agency (SOC 156)-2.pdf";
+    public static final String FP_Agreement_Child_by_Agency_SOC_156_2_header = "AGENCY — FOSTER PARENTS AGREEMENT";
+
+    public static final String FP_Placement_Agreement_for_NMD_by_Agency_SOC_156A = "FP Placement Agreement for NMD by Agency (SOC 156A)";
+    public static final String FP_Placement_Agreement_for_NMD_by_Agency_SOC_156A_Path = "C:/Users/suponugupati/Documents/State of CA/POC/PDF Files/FP Placement Agreement for NMD by Agency (SOC 156A).pdf";
+    public static final String FP_Placement_Agreement_for_NMD_by_Agency_SOC_156A_Header = "AGENCY — FOSTER PARENT(S) AGREEMENT";
 }
